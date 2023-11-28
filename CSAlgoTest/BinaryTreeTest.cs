c1b9552d7b26509ec32954662b85d0c44f69895a
PiotrB <piotrb@gmail.com>
2023-11-28 11:59:19 +0100

    Binary tree test preparation

---
+++ CSAlgoTest/BinaryTreeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAlgoTest
+{
+    public class BinaryTreeTest
+    {
+        [Fact]
+        public void EmptyTreeWhenZeroElement()
+        {
+            //var tree = new CSAlgoLibrary.BinaryTree();
+            //Assert.Equal(0, tree.Count);
+        }
+    }
+}
